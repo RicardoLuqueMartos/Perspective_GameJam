ac4093357d67e62eb09fbbac8acd8d1c7c69560a
chef13 <julien_caulier@live.fr>
2025-07-17 13:39:01 +0200

    Player Interact

---
+++ Assets/Scrypts/scrypt player/PlayerInteract.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerInteract : MonoBehaviour
+{
+    [SerializeField] private float interactDistance = 1f;
+    [SerializeField] private LayerMask interactable;
+    private RaycastHit hit;
+    private IInteractable currentTarget;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //hit = new RaycastHit();
+        Physics.Raycast(transform.position , transform.forward, out hit, interactDistance, interactable);
+        if (hit.collider != null)
+        {
+
+                currentTarget = hit.collider.GetComponent<IInteractable>();
+                currentTarget.IsInteractable(hit);
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    currentTarget.Interact();
+                }
+           
+        }
+        else
+        {
+            currentTarget = null;
+            UiManager.Instance.contectuelInteracted("");
+        }
+    }
+}
