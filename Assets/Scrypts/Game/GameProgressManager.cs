21858f931d0446b205c667422c85ec1163f18794
RicardoLuqueMartos <121223508+RicardoLuqueMartos@users.noreply.github.com>
2025-07-18 15:01:07 +0200

    Tesseract

---
+++ Assets/Scrypts/Game/GameProgressManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class GameProgressManager : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private int tesseractAmount = 0;
+
+
+    public static GameProgressManager instance;
+
+#endregion Variables
+
+    #region Init
+    private void Awake()
+    {
+        CreateInstance();
+    }
+
+    void CreateInstance()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    #endregion Init
+
+    private void OnEnable()
+    {
+    //    ManageTesseractDisplay();
+    }
+
+    public void PlayerGetTesseract()
+    {
+        tesseractAmount++;
+        ManageTesseractDisplay();
+    }
+
+    public void PlayerUseTesseract()
+    {
+        tesseractAmount--;
+        ManageTesseractDisplay();
+    }
+
+    public void ManageTesseractDisplay()
+    {
+        UiManager.instance.DisplayTesseract(HaveTesseract());
+    }
+
+    public bool HaveTesseract()
+    {
+        bool response = false;
+        if (tesseractAmount > 0) response = true;
+        return response;
+    }
+}
