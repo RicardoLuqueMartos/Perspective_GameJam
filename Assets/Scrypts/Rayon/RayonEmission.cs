b6e132ae7dfbecdbe398616e5fd4b4c98efd8cf3
chef13 <julien_caulier@live.fr>
2025-07-19 19:40:45 +0200

    corection decalage hit surface/vide

--- Assets/Scrypts/Rayon/RayonEmission.cs
+++ Assets/Scrypts/Rayon/RayonEmission.cs
@@ -60,9 +60,10 @@ public class RayonEmission : MonoBehaviour
                     while (true)
                     {
                         Vector3 newDirection = Vector3.Reflect(currentDirection, lastHit.normal);
-                        Debug.Log("Rayon Direction: " + newDirection);
-                        Physics.Raycast(lastHit.point, newDirection, out RaycastHit newhit);
-                        Debug.DrawRay(lastHit.point, lastHit.normal, Color.red, 2f);
+                        //Debug.Log("Rayon Direction: " + newDirection);
+                        Vector3 startPoint = lastHit.point + newDirection * 0.01f; // Offset pour éviter le décalage
+                        Physics.Raycast(startPoint, newDirection, out RaycastHit newhit);
+                        //Debug.DrawRay(lastHit.point, lastHit.normal, Color.red, 2f);
                         if (newhit.collider != null && !newhit.collider.CompareTag("Reflecteur"))
                         {
                             hits.Add(newhit);
@@ -110,8 +111,8 @@ public class RayonEmission : MonoBehaviour
                     // Si le rayon finit sans impact
                     if (endWithOutHit)
                     {
-
-                        rayonRenderer.SetPosition(hits.Count + 1, directionWithOutHit * 100f);
+                        Vector3 lastPoint = hits[hits.Count - 1].point;
+                        rayonRenderer.SetPosition(hits.Count + 1, lastPoint + directionWithOutHit * 100f);
                         if (currentReccepteurRayon != null)
                         currentReccepteurRayon.SetPowered(false);
                         currentReccepteurRayon = null;
