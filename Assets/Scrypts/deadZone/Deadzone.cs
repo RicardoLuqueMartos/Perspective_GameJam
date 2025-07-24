a2810757228bd97e92d849e794f3345ab5f99d16
chef13 <julien_caulier@live.fr>
2025-07-24 22:44:08 +0200

    refactor RBPlayer

--- Assets/Scrypts/deadZone/Deadzone.cs
+++ Assets/Scrypts/deadZone/Deadzone.cs
@@ -31,5 +31,36 @@ public class Deadzone : MonoBehaviour
         }
     }
 
-    
+    public void KillPlayer()
+    {
+        StartCoroutine(FadeOutToRespawn());
+    }
+    public IEnumerator FadeOutToRespawn()
+    {
+        UiManager.instance.fadingPanel.enabled = true;
+        UiManager.instance.fadingPanel.DOFade(1, 1);
+        RBPlayer.instance.material.DOFloat(1.1f, "_dissolveAmount", 2f).SetEase(Ease.InOutQuad); // Start with dissolve effect
+        SoundLauncher.instance.PlayDissolve();
+
+
+        yield return new WaitForSeconds(2);
+
+        // Désactivation du CharacterController avant de déplacer le joueur
+        var controller = RBPlayer.instance;
+        if (controller != null) controller.enabled = false;
+
+        RBPlayer.instance.gameObject.transform.position = GameProgressManager.instance.currentRespawnTransform.position;
+
+        // Réactivation du CharacterController après le déplacement
+        if (controller != null) controller.enabled = true;
+
+        UiManager.instance.fadingPanel.DOFade(0, 2);
+        RBPlayer.instance.material.DOFloat(0.1f, "_dissolveAmount", 2f).SetEase(Ease.InOutQuad); // resummoning disolve
+        SoundLauncher.instance.PlayDissolve();
+        yield return new WaitForSeconds(2);
+        UiManager.instance.fadingPanel.enabled = false;
+        yield break;
+    }
+
+
 }
